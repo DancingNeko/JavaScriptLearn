f00a1b1f9b27f762a0bdcf686e1f33f3caeecdaa
Daniel PC <fengzhenyang47@gmail.com>
2022-02-16 22:39:06 -0500

    change timer to a rgb timer :)

--- Timer.js
+++ Timer.js
@@ -1,5 +1,28 @@
-count = 0;
+let minute = 0;
+let second = 0;
+let centisecond = 0;
+let hue = 0;
+document.body.style.fontSize = "xx-large"
+function castNumber(num)
+{
+    return (num<10) ? ("0"+num):num;
+}
+
 setInterval(() => {
-    count ++;
-    document.body.innerHTML = (count + "s<br>"); 
-}, 1000);
+    hue++;
+    centisecond++;
+    if(second == 60){
+        minute++;
+        second = 0;
+    }
+    if(centisecond == 100)
+    {
+        second ++;
+        centisecond = 0;
+    }
+    if(hue == 360)
+        hue = 0;
+    document.body.innerHTML = (`${castNumber(minute)}:${castNumber(second)}:${castNumber(centisecond)} elapsed`); 
+    document.body.style.backgroundColor = `hsl(${hue}, 100%, 90%)`
+    document.body.style.color = `hsl(${360-hue}, 100%, 40%)`
+}, 10);
