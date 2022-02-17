b7704d411c9f28eff255dcff59952b691d2af20f
Zhenyang Feng <feng.6@C02NX1LDG083.uaschools.org>
2022-02-17 10:59:20 -0500

    upgrade timer

--- Timer.js
+++ Timer.js
@@ -2,6 +2,7 @@ let minute = 0;
 let second = 0;
 let centisecond = 0;
 let hue = 0;
+let dots = '<br>';
 document.body.style.fontSize = "xx-large"
 function castNumber(num)
 {
@@ -10,6 +11,7 @@ function castNumber(num)
 
 setInterval(() => {
     hue++;
+    dots += ".";
     centisecond++;
     if(second == 60){
         minute++;
@@ -19,10 +21,11 @@ setInterval(() => {
     {
         second ++;
         centisecond = 0;
+        dots += "<br>"
     }
     if(hue == 360)
         hue = 0;
-    document.body.innerHTML = (`${castNumber(minute)}:${castNumber(second)}:${castNumber(centisecond)} elapsed`); 
+    document.body.innerHTML = (`${castNumber(minute)}:${castNumber(second)}:${castNumber(centisecond)} elapsed ${dots}`); 
     document.body.style.backgroundColor = `hsl(${hue}, 100%, 90%)`
     document.body.style.color = `hsl(${360-hue}, 100%, 40%)`
 }, 10);
