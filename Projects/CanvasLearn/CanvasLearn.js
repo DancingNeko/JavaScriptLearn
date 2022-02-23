089bf0b01a17b67b6a3528662669f4b8b05bc127
Zhenyang Feng <feng.6@C02NX1LDG083.local>
2022-02-22 22:38:00 -0500

    javascript canvas learn

---
+++ Projects/CanvasLearn/CanvasLearn.js
@@ -0,0 +1,55 @@
+
+document.body.style.backgroundColor = "rgb(0,0,0)";
+let canvas = document.getElementById("draw");
+let ctx = canvas.getContext("2d");
+tick = 0; // record setInterval ticks
+circles = [];
+
+function paint(){
+    tick++;
+    ctx.clearRect(0,0,canvas.width,canvas.height);
+    chaos();
+    for(let i = 0; i < circles.length; i++){
+        circles[i].draw();
+    }
+}
+
+
+function prepareCircle(num) {
+    for(let i = 0; i < num; i++){
+        circles.push(new glowCircle(parseInt(Math.random()*400),parseInt(Math.random()*canvas.width),parseInt(Math.random()*canvas.height)));
+    }
+}
+
+class glowCircle{
+    constructor(shift,x,y){
+        this.shift = shift;
+        this.x = x;
+        this.y = y;
+        console.log(shift);
+    }
+    draw() {   
+        let size = Math.abs((tick+this.shift)%40-20); // shift back and forth between 0 and 10
+        for(let i = 0; i < size; i++){
+            ctx.fillStyle = "rgba(255,255,255,0.01)";
+            ctx.beginPath();
+            ctx.arc(this.x, this.y, i*2, 0, 2 * Math.PI);
+            ctx.fill();
+        } 
+    }
+}
+
+function chaos(){
+    for(let i = 0; i < 50; i++){
+    ctx.fillStyle = `hsl(${parseInt(Math.random()*360)}, 100%, 40%)`;
+    ctx.fillRect(parseInt(Math.random()*canvas.width),parseInt(Math.random()*canvas.height),parseInt(Math.random()*60),parseInt(Math.random()*60));
+    }
+}
+
+
+
+
+
+
+prepareCircle(100);
+setInterval(paint, 100);
